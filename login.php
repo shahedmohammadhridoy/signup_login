3d9328f98f00bbf970d2eb983276b54c69e1ec35
Shahed <shahed_hridoy@outlook.com>
2022-08-17 23:03:54 +0600

    login page

---
+++ login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">\
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+    <form action="" class="shadow w-450 ">
+        <h4 class="display-4 fs-1">Login</h4>
+        <div class="mb-3">
+            <label>User name</label>
+            <input type="text" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label>Password</label>
+            <input type="password">
+        </div>
+        <button>Login</button>
+        <a href=""></a>
+    </form>
+</body>
+</html>
