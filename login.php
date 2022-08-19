ef43ff46f149ff22c6f00ba322d3489d086cd45b
Shahed <shahed_hridoy@outlook.com>
2022-08-19 20:23:14 +0600

    login

--- login.php
+++ login.php
@@ -5,22 +5,24 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">\
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
-    <form action="" class="shadow w-450 ">
-        <h4 class="display-4 fs-1">Login</h4>
-        <div class="mb-3">
-            <label>User name</label>
-            <input type="text" class="form-control">
-        </div>
-        <div class="mb-3">
-            <label>Password</label>
-            <input type="password">
-        </div>
-        <button>Login</button>
-        <a href=""></a>
-    </form>
+    <div class="d-flex justify-content-center align-items-center vh-100">
+        <form action="php/login.php" class="shadow w-450 p-3">
+            <h4 class="display-4 fs-1">Login</h4>
+            <div class="mb-3">
+                <label class="form-label">User name</label>
+                <input type="text" class="form-control">
+            </div>
+            <div class="mb-3">
+                <label class="form-label">Password</label>
+                <input type="password" class="form-control">
+            </div>
+            <button class="btn btn-primary">Login</button>
+            <a href="index.php" class="link-secondary">Sign Up</a>
+        </form>
+    </div>
 </body>
 </html>
