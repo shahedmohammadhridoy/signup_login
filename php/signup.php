39713550eb165fe0320e0dbd13e09b55cedb00b8
Shahed <shahed_hridoy@outlook.com>
2022-08-21 23:08:31 +0600

    sign up api

--- php/signup.php
+++ php/signup.php
@@ -1,6 +1,15 @@
 <?php
 
-echo $_POST['fname'];
-echo $_POST['uname'];
-echo $_POST['pass'];
-echo $_POST['pp'];
+if( isset($_POST['fname']) && isset($_POST['uname']) && isset($_POST['pass'])){
+    
+    include '../db_conn.php';
+
+    $fname = $_POST['fname'];
+    $uname = $_POST['uname'];
+    $pass = $_POST['pass'];
+
+    $data = "fname=".$fname."&username=".$uname;
+   
+
+
+}
