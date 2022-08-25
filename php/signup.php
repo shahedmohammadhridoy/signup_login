ae79fc2c79cf2b8d14ee14cdcbb945a58d333462
Shahed <shahed_hridoy@outlook.com>
2022-08-25 22:49:27 +0600

    access forbidden img folder

--- php/signup.php
+++ php/signup.php
@@ -30,10 +30,41 @@ if( isset($_POST['fname']) && isset($_POST['uname']) && isset($_POST['pass'])){
 
             $img_name = $_FILES['pp']['name'];
             $tmp_name = $_FILES['pp']['tmp_name'];
+            $error = $_FILES['pp']['error'];
 
+            if($error === 0){
+                $img_ex = pathinfo($img_name,PATHINFO_EXTENSION);
+                $img_ex_to_lc = strtolower($img_ex);
+
+
+                $allowed_exs = array('jpg','jpeg','png');
+                if(in_array($img_ex_to_lc,$allowed_exs)){
+
+                    $new_img_name = uniqid($uname,TRUE).'.'.$img_ex_to_lc;
+                    $img_upload_path = '../upload/'.$new_img_name;
+                    move_uploaded_file($tmp_name,$img_upload_path);
+
+                    //Insert into database
+                    $sql = "INSERT INTO users(fname, username, password, pp)
+                    VALUES(?,?,?,?)";
+                    $stmt = $conn->prepare($sql);
+                    $stmt->execute([$fname,$uname,$pass,$new_img_name]);
+
+                    header("Location: ../index.php?success=Your account has been created successfully");
+                    exit;
+                    
+                } else {
+                    $em = "You can't upload files of this type";
+                    header("Location: ../index.php?error=$em&$data");
+                    exit;
+                }
+            } else {
+                $em = "Unknown error occurred!";
+                header("Location: ../index.php?$em&$data");
+                exit;
+            }
+        } else {
             
-
         }
-
     }
 }
---
+++ upload/index.php
@@ -0,0 +1,4 @@
+<?php
+echo '<title>403 : Forbidden</title>';
+echo '<h1>Forbidden</h1>';
+echo '<p>You don\'t have permission to access this resource.</p>';
